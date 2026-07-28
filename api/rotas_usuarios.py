a0c78bc29dd432db9f7eb4c37774d965b01c6383
Dérick Raphael <derick.r.silva6@aluno.senai.br>
2026-07-28 11:39:32 -0400

    Dérick Raphael

--- api/rotas_usuarios.py
+++ api/rotas_usuarios.py
@@ -3,7 +3,7 @@ from sqlalchemy.orm import Session
 from database import get_db
 from models.usuario import Usuario
 from schemas.usuario_schema import UsuarioCreate, UsuarioResponse
-from core.security import verificar_admin
+from core.security import verificar_admin, verificar_acesso_basico
 
 router = APIRouter(prefix="/usuarios", tags=["Usuários"])
 
@@ -19,6 +19,35 @@ def criar_usuario(usuario: UsuarioCreate, db: Session = Depends(get_db)):
     db.commit()
     db.refresh(novo_usuario)
     return novo_usuario
+
+#Meu ajuste
+@router.get("/", response_model = list[UsuarioResponse])
+def Listar_usuarios(
+    db: Session = Depends(get_db),
+    usuario_logado: Usuario = Depends(verificar_acesso_basico)
+):
+    usuarios = db.query(Usuario).all()
+    return usuarios
+
+@router.put("/{usuario_id}", response_model=UsuarioResponse)
+def atualizar_usuario(
+    usuario_id: int,
+    usuario_atualizado: UsuarioCreate,
+    db: Session = Depends(get_db),
+    usuario_logado: Usuario = Depends(verificar_acesso_basico)
+):
+    usuario = db.query(Usuario).filter(Usuario.id == usuario_id).fister()
+    if not usuario:
+        raise HTTPException(status_code=404, detail="Usuário não encontrado")
+    
+    usuario.nome = usuario_atualizado.nome
+    usuario.username = usuario_atualizado.username
+    usuario.senha_hash = usuario_atualizado.senha
+    usuario.tipo = usuario_atualizado.tipo
+    
+    db.commit()
+    db.refresh(usuario)
+    return usuario
 
 @router.delete("/{usuario_id}", status_code=status.HTTP_204_NO_CONTENT)
 def deletar_usuario(
